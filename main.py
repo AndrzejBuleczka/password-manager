2c2d7c7e5ef0a9a18a2253e283ad8586a33a27b8
Andrzej Bułeczka <andrzejbuleczka@gmail.com>
2024-06-21 15:32:39 +0200

    refactor save function

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from random import choice, randint, shuffle
 import pyperclip
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
-#Password Generator Project
+# Password Generator Project
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
@@ -42,8 +42,6 @@ def save():
             website_input.delete(0, END)
             password_input.delete(0, END)
             messagebox.showinfo(title="Success", message="Password has been saved!")
-        else:
-            return
 
 
 # ---------------------------- UI SETUP ------------------------------- #
